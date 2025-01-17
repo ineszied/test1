5a51ccb19d1b13dc6a2e673d17ae919463158c70
ziedbenahmed <zied.ben.ahmed1@gmail.com>
2025-01-17 09:39:35 +0100

    premier commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 17 09:29:46 2025
+
+@author: Windows
+"""
+
+# Script para imprimir "Hello, World!" en la consola
+def main():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
